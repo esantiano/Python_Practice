01500ef7159c61cfbbca356466e0ce21d24c4d0d
esantiano <103700795+esantiano@users.noreply.github.com>
2025-11-22 15:49:11 -0500

    TopKFrequentElements non quick sort approach practice

---
+++ LeetCode/Hashing/Map/Tests/__pycache__/test_TopKFrequentElements.py
@@ -0,0 +1,12 @@
+import pytest
+from TopKFrequentElements import Solution
+
+def test_basic():
+    nums=[1,1,1,2,2,3]
+    k=2
+    assert Solution().topKFrequent(nums,k)==[1,2]
+
+def test_edge_single():
+    nums=[1]
+    k=1
+    assert Solution().topKFrequent(nums,k)==[1]
